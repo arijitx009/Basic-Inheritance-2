621b57485f192681e2359a2966d87919dd4c89dd
arijitx009 <41569612+arijitx009@users.noreply.github.com>
2018-08-04 01:58:04 +0530

    Add files via upload

---
+++ Basic Inheritance 2/src/Member.java
@@ -0,0 +1,20 @@
+
+public class Member {
+		
+	int nstatic=9;
+	static int ystatic=8;
+	
+	static void method1()									//Static method
+	{
+		System.out.println("Static method" +ystatic);
+	}
+	
+	void method2()											//Non-static method
+	{
+		System.out.println("Non-static method" +nstatic);
+		
+	}
+	
+
+	
+}
---
+++ Basic Inheritance 2/src/Test.java
@@ -0,0 +1,15 @@
+
+
+	
+	public class Test {
+
+		public static void main(String args[])
+		{
+			Member.method1();								//Calling static method
+			Member memberobj = new Member();				//creating an object so as to call the non static method
+			memberobj.method2();							//Calling non-static method
+			
+		}
+	}
+
+
